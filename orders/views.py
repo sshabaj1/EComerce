0e1a7fdb5a73055b377e124d66375d7ee93ad325
Senad Shabaj <senadshabaj73@gmail.com>
2021-11-04 21:36:24 +0100

    creating pdf/ bug at installing packages/ need to install app libraries

--- orders/views.py
+++ orders/views.py
@@ -9,7 +9,10 @@ from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
 from .models import Order
-
+import weasyprint
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
 
@@ -57,3 +60,16 @@ def admin_order_detail(request, order_id):
     return render(request,
                   'admin/orders/order/detail.html',
                   {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
